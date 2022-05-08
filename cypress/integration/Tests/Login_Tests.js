c096daf9ff52593b6d4d201b651a3c39e0635bfa
Ibrahim Garuba <garubaibrahim005@gmail.com>
2022-05-08 19:20:18 +0100

    Test

---
+++ cypress/integration/Tests/Login_Tests.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+describe ("Test login functionalities", function () {
+
+    it ("Successful Login", function() {
+
+        cy.visit("https://www.saucedemo.com/")
+
+    })
+
+
+})
